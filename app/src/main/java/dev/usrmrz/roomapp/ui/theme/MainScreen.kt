81f9bd22ecd981fb26abc493070ec64e2932ea3b
usrmrz <usrmrz@gmail.com>
2024-09-05 01:04:16 +0300

    add Spacer and LazyColumn

--- app/src/main/java/dev/usrmrz/roomapp/ui/theme/MainScreen.kt
+++ app/src/main/java/dev/usrmrz/roomapp/ui/theme/MainScreen.kt
@@ -3,8 +3,11 @@ package dev.usrmrz.roomapp.ui.theme
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
@@ -17,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview(showBackground = true)
 @Composable
@@ -53,6 +57,16 @@ fun MainScreen() {
                     contentDescription = "Add"
                 )
             }
+        }
+        Spacer(
+            modifier = Modifier
+                .height(5.dp)
+        )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+
         }
     }
 }
