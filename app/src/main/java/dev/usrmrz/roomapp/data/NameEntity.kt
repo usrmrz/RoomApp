a81f283e19c3234df1adf639489dcfae47eaf55f
usrmrz <usrmrz@gmail.com>
2024-09-06 12:53:12 +0300

    renaming database elements

--- app/src/main/java/dev/usrmrz/roomapp/data/NameEntity.kt
+++ app/src/main/java/dev/usrmrz/roomapp/data/NameEntity.kt
@@ -1,11 +1,14 @@
 package dev.usrmrz.roomapp.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "roomapp_table")
+@Entity(tableName = "entities_table")
 data class NameEntity(
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "entities_table_id")
     val id: Int? = null,
+    @ColumnInfo(name = "entities_table_name")
     val name: String
 )
